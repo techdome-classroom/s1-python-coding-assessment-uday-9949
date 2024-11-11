a00c8e58dc94801fe007235c527833054b57858c
PULI UDAY KIRAN <119495444+uday-9949@users.noreply.github.com>
2024-11-11 16:35:49 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,19 +1,18 @@
 import unittest
-from program2 import decode_message
+from program2 import Decoder
 
 class TestDecoder(unittest.TestCase):
     def test_case1(self):
-        self.assertEqual(decode_message("aa", "a"), False)
+        self.assertEqual(Decoder.decode_message("aa", "a"), False)
 
     def test_case2(self):
-        self.assertEqual(decode_message("aa", "*"), True)
+        self.assertEqual(Decoder.decode_message("aa", "*"), True)
 
     def test_case3(self):
-        self.assertEqual(decode_message("cb", "?a"), False)
+        self.assertEqual(Decoder.decode_message("cb", "?a"), False)
 
     def test_case4(self):
-        self.assertEqual(decode_message("abc", "?b?"), True)
+        self.assertEqual(Decoder.decode_message("abc", "?b?"), True)
 
 if __name__ == '__main__':
     unittest.main()
-
