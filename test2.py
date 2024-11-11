14e3c660a1fe0e84fcf0e5287877c568bfff4fc2
PULI UDAY KIRAN <119495444+uday-9949@users.noreply.github.com>
2024-11-11 16:39:53 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,24 +1,28 @@
 import unittest
-from program2 import Decoder  # Assuming the class Decoder is in program2.py
+from program2 import Solution
 
-class TestDecoder(unittest.TestCase):
+class TestSolution(unittest.TestCase):
     def setUp(self):
-        self.decoder = Decoder()  # Instantiate the Decoder class
+        self.solution = Solution()
 
     def test_case1(self):
-        result = self.decoder.decode_message("aa", "a")
+        result = self.solution.decode_message("aa", "a")
         self.assertEqual(result, False)
 
     def test_case2(self):
-        result = self.decoder.decode_message("aa", "*")
+        result = self.solution.decode_message("aa", "*")
         self.assertEqual(result, True)
 
     def test_case3(self):
-        result = self.decoder.decode_message("cb", "?a")
+        result = self.solution.decode_message("cb", "?a")
         self.assertEqual(result, False)
 
     def test_case4(self):
-        result = self.decoder.decode_message("abc", "?b?")
+        result = self.solution.decode_message("abc", "?b?")
+        self.assertEqual(result, True)
+
+    def test_case5(self):
+        result = self.solution.decode_message("ab", "ab*")
         self.assertEqual(result, True)
 
 if __name__ == '__main__':
