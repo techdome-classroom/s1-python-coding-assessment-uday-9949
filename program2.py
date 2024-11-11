62d064540933894c8d594e16710f5c65469953ad
PULI UDAY KIRAN <119495444+uday-9949@users.noreply.github.com>
2024-11-11 13:42:26 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,5 +1,17 @@
-import re
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        dp = [[False] * (len(p) + 1) for _ in range(len(s) + 1)]
+        dp[0][0] = True
 
-def decode_message(s: str, p: str) -> bool:
-    p = p.replace("?", ".").replace("*", ".*")
-    return bool(re.fullmatch(p, s))
+        for j in range(1, len(p) + 1):
+            if p[j - 1] == '*':
+                dp[0][j] = dp[0][j - 1]
+        
+        for i in range(1, len(s) + 1):
+            for j in range(1, len(p) + 1):
+                if p[j - 1] in {s[i - 1], '?'}:
+                    dp[i][j] = dp[i - 1][j - 1]
+                elif p[j - 1] == '*':
+                    dp[i][j] = dp[i][j - 1] or dp[i - 1][j]
+        
+        return dp[len(s)][len(p)]
